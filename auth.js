b2a18dd794ddefd6e8ca38ff09a0577786c111b9
Ronaldo_Op <brandonprimo37@hotmail.com>
2025-02-17 19:32:19 -0600

    supabase

--- auth.js
+++ auth.js
@@ -23,7 +23,7 @@ function validarPassword(password) {
     return regex.test(password);
 }
 
-let intentosFallidos = 0; // Para bloquear múltiples intentos fallidos
+// let intentosFallidos = 0; // Para bloquear múltiples intentos fallidos
 
 // Función para registrar usuario con validaciones
 document.getElementById("register-btn").addEventListener("click", async () => {
@@ -72,7 +72,7 @@ document.getElementById("login-btn").addEventListener("click", async () => {
         }, 2000);
     }
 });
-
+/*
 // Mantener sesión iniciada
 async function verificarSesion() {
     const { data } = await supabase.auth.getSession();
