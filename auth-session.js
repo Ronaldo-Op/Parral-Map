d48d55c415e4fa0f62a31c668548a79f2de0afc1
Ronaldo_Op <brandonprimo37@hotmail.com>
2025-02-20 17:14:14 -0600

    supabase

--- auth-session.js
+++ auth-session.js
@@ -278,7 +278,7 @@ async function subirGeoJSON() {
                         surface: surface,
                         coordinates: coordinates,
                         color: '#0000FF', // Color inicial (Azul)
-                        estado: 'Desconocido'
+                        state: 'Desconocido'
                     }
                 ]);
 
